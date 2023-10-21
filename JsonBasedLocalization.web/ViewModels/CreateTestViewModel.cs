470b0ae15662a0cd02f55f19eebacd90c7ca6509
MuhammadJamal99 <muhammadjammi18@gmail.com>
2023-10-21 13:05:48 +0300

    dev End Of Viedo 14

---
+++ JsonBasedLocalization.web/ViewModels/CreateTestViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JsonBasedLocalization.web.ViewModels;
+
+public sealed class CreateTestViewModel
+{
+    [Display(Name = "name"), Required(ErrorMessage = "required")]
+    public string Name { get; set; }
+}
